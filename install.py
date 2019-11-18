625207a2a53e895bd26a651d89613810e5e0da96
CKOwen <44505297+OwenCK@users.noreply.github.com>
2019-11-18 15:03:49 +0000

    Force kill any frozen studio instances

--- install.py
+++ install.py
@@ -59,6 +59,10 @@ def killStudioProcess():
     os.system("taskkill /im RobloxStudioBeta.exe")
 
 
+def forceKillStudioProcess():
+    os.system("taskkill /f /im RobloxStudioBeta.exe")
+
+
 def waitForContentPath():
     # The content path is used by applications like run-in-roblox to identify Studio's install directory
     # As it is not created until studio closes, we need to wait for it
@@ -101,3 +105,4 @@ print('\nCreating plugins directory', flush=True)
 createPluginsDirectory()
 
 print('\nStudio installed and authenticated', flush=True)
+forceKillStudioProcess()
